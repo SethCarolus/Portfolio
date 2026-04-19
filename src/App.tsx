4938da65f3d2125adcd17144f3bec4ac2081431a
Seth Carolus <sethcarolus.software@gmail.com>
2026-04-19 03:14:03 +0200

    Added Tetris Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import './App.css'
-import Header from './components/Header'
-import Hero from './components/Hero'
-import Projects from './components/Projects'
-import Footer from './components/Footer'
-
+import "./App.css";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
-      <Header/>
-      <Hero/>
-      <center><h2>Projects</h2></center>
-      <Projects/>
-      <Footer/>
+      <Header />
+      <Hero />
+      <center>
+        <h2>Projects</h2>
+      </center>
+      <Projects />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
